a51d2c9de8736794d318df807218c31733413847
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-27 17:21:26 +0100

    added operations

--- operations.c
+++ operations.c
@@ -5,8 +5,9 @@ void sa(t_stack **a, t_stack **b)
 	int	size_a;
 	int	first;
 	int	second;
-	int	size_b;
+	int	count;
 
+	count = 0;
 	size_a = ft_stack_size(*a);
 	if(size_a < 3)
 		return ;
@@ -14,11 +15,11 @@ void sa(t_stack **a, t_stack **b)
 	{
 		ft_push(b, ft_new_stack(ft_pop(a)));
 		size_a--;
+		count++;
 	}
 	first = ft_pop(a);
 	second = ft_pop(a);
-	size_b = ft_stack_size(*b);
-	while(size_b--)
+	while(count--)
 		ft_push(a, ft_new_stack(ft_pop(b)));
 	ft_push(a, ft_new_stack(second));
 	ft_push(a, ft_new_stack(first));
@@ -26,11 +27,12 @@ void sa(t_stack **a, t_stack **b)
 
 void sb(t_stack **a, t_stack **b)
 {
-	int	size_a;
+	int	count;
 	int	first;
 	int	second;
 	int	size_b;
 
+	count = 0;
 	size_b = ft_stack_size(*b);
 	if(size_b < 3)
 		return ;
@@ -38,12 +40,128 @@ void sb(t_stack **a, t_stack **b)
 	{
 		ft_push(a, ft_new_stack(ft_pop(b)));
 		size_b--;
+		count++;
 	}
 	first = ft_pop(b);
 	second = ft_pop(b);
-	size_a = ft_stack_size(*a);
-	while(size_a--)
+	while(count--)
 		ft_push(b, ft_new_stack(ft_pop(a)));
 	ft_push(b, ft_new_stack(second));
 	ft_push(b, ft_new_stack(first));
+}
+
+void	pa(t_stack **a, t_stack **b)
+{
+	if(!b || !*b)
+		return ;
+	ft_push(a, ft_new_stack(ft_pop(b)));
+}
+
+void	pb(t_stack **a, t_stack **b)
+{
+	if(!a || !*a)
+		return ;
+	ft_push(b, ft_new_stack(ft_pop(a)));
+}
+
+void	ra(t_stack **a, t_stack **b)
+{
+	int	first;
+	int	size_a;
+	int	count;
+
+	count = 0;
+	size_a = ft_stack_size(*a);
+	if(size_a <= 1)
+		return ;
+	while(size_a > 1)
+	{
+		pb(a, b);
+		size_a--;
+		count++;
+	}
+	first = ft_pop(a);
+	while(count--)
+		pa(a, b);
+	ft_push(a, ft_new_stack(first));
+}
+
+void	rb(t_stack **a, t_stack **b)
+{
+	int	first;
+	int	count;
+	int	size_b;
+
+	count = 0;
+	size_b = ft_stack_size(*b);
+	if(size_b <= 1)
+		return ;
+	while(size_b > 1)
+	{
+		pa(a, b);
+		size_b--;
+		count++;
+	}
+	first = ft_pop(b);
+	while(count--)
+		pb(a, b);
+	ft_push(b, ft_new_stack(first));
+}
+
+void	rr(t_stack **a, t_stack **b)
+{
+	ra(a, b);
+	rb(a, b);
+}
+
+void	rra(t_stack **a, t_stack **b)
+{
+	int	last;
+	int	count;
+	int	size_a;
+
+	count = 0;
+	size_a = ft_stack_size(*a);
+	if(size_a <= 1)
+		return ;
+	last = ft_pop(a);
+	size_a--;
+	while(size_a > 0)
+	{
+		pb(a, b);
+		size_a--;
+		count++;
+	}
+	ft_push(a, ft_new_stack(last));
+	while(count--)
+		pa(a, b);
+}
+
+void	rrb(t_stack **a, t_stack **b)
+{
+	int	last;
+	int	count;
+	int	size_b;
+
+	count = 0;
+	size_b = ft_stack_size(*b);
+	if(size_b <= 1)
+		return ;
+	last = ft_pop(b);
+	size_b--;
+	while(size_b > 0)
+	{
+		pa(a, b);
+		size_b--;
+		count++;
+	}
+	ft_push(b, ft_new_stack(last));
+	while(count--)
+		pb(a, b);
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	rra(a, b);
+	rrb(a, b);
 }
--- push_swap.h
+++ push_swap.h
@@ -18,5 +18,15 @@ int	ft_stack_size(t_stack *stack);
 int	ft_pop(t_stack **stack);
 void	ft_stack_clear(t_stack **stack);
 void	ft_print_stack(t_stack *stack);
+void	sa(t_stack **a, t_stack **b);
+void	sb(t_stack **a, t_stack **b);
+void	pa(t_stack **a, t_stack **b);
+void	pb(t_stack **a, t_stack **b);
+void	ra(t_stack **a, t_stack **b);
+void	rb(t_stack **a, t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+void	rra(t_stack **a, t_stack **b);
+void	rrb(t_stack **a, t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
 
 #endif
