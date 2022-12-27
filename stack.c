8066ec8be8089358954f677e8d04ac294d9b05ef
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-27 17:21:01 +0100

    Improved complexity of push and pop from O(n) to O(1)

--- stack.c
+++ stack.c
@@ -26,10 +26,9 @@ void	ft_push(t_stack **stack, t_stack *new)
 		return ;
 	}
 	tmp = *stack;
-	while(tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = new;
-	new->previous = tmp;
+	new->next = tmp;
+	tmp->previous = new;
+	*stack = new;
 }
 
 int	ft_stack_size(t_stack *stack)
@@ -49,16 +48,14 @@ int	ft_stack_size(t_stack *stack)
 
 int	ft_pop(t_stack **stack)
 {
-	t_stack	*tmp;
+	t_stack *tmp;
 	int	ret;
 
-	if(!*stack || !stack)
-		exit(EXIT_FAILURE);
 	tmp = *stack;
-	while(tmp->next != NULL)
-		tmp = tmp->next;
-	if(tmp->previous != NULL)
-		tmp->previous->next = tmp->next;
+	*stack = (*stack)->next;
+	if(tmp->next != NULL)
+		tmp->next->previous = NULL;
+	tmp->next = NULL;
 	ret = tmp->content;
 	free(tmp);
 	return (ret);
@@ -71,14 +68,51 @@ void	ft_stack_clear(t_stack **stack)
 	size = ft_stack_size(*stack);
 	while(size--)
 		ft_pop(stack);
+	*stack = NULL;
 }
 
 void	ft_print_stack(t_stack *stack)
 {
+	if(!stack)
+	{
+		printf("NULL\n");
+		return ;
+	}
+	while(stack->next != NULL)
+		stack = stack->next;
 	while(stack != NULL)
 	{
-		printf("%d, ", stack->content);
-		stack = stack->next;
+		printf("%d ", stack->content);
+		stack = stack->previous;
 	}
 	printf("\n");
 }
+/*
+int	main()
+{
+	t_stack	*b = ft_new_stack(1);
+	t_stack	*a = ft_new_stack(7);
+
+	ft_push(&b, ft_new_stack(2));
+	ft_push(&b, ft_new_stack(3));
+	ft_push(&b, ft_new_stack(4));
+	ft_push(&b, ft_new_stack(5));
+	ft_push(&b, ft_new_stack(6));
+	printf("b: ");
+	ft_print_stack(b);
+
+	ft_push(&a, ft_new_stack(8));
+	ft_push(&a, ft_new_stack(9));
+	ft_push(&a, ft_new_stack(10));
+	ft_push(&a, ft_new_stack(11));
+	ft_push(&a, ft_new_stack(12));
+	printf("a: ");
+	ft_print_stack(a);
+	rrr(&a, &b);
+	printf("a: ");
+	ft_print_stack(a);
+	printf("b: ");
+	ft_print_stack(b);
+
+	return (0);
+}*/
